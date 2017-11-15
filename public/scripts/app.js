fb56dc6a9a1bb4f9bff7653f30d08f84a82d6b44
chrynuik <catherine.hrynuik@gmail.com>
2017-11-15 03:05:53 +0000

    added scripts to look through data oject and display tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,31 +6,64 @@
 
 
 $(document).ready(function(){
-  var tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  var data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
       },
-      "handle": "@SirIsaac"
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants <script>alert('Vulnerable')</script>"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
     },
-    "created_at": 1461116232227
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
   }
 
-  function createTweetElement(tweet) {
+  function createTweetElement(tweetData) {
 
-    // var $tweetImage = $('<img>').attr('src', );
-    // var $tweetTitle = $('<h1>').text(tweetData.user.name);
-    // var $tweetHeader = $('<header>').append($tweetImage).append($tweetTitle);
-    // var $tweet = $("<article>").addClass("tweet").append($tweetHeader);
-
-    //return $tweet;
     var tweetTime = Math.floor((Date.now() - tweetData.created_at) / (1000*60*60*24));
+    var tweetText = escape(tweetData.content.text);
+    console.log(tweetData);
+
     return `<article class="tweet">
           <header>
             <img src="${tweetData.user.avatars.small}">
@@ -38,7 +71,7 @@ $(document).ready(function(){
             <div class="tweet-handle">${tweetData.user.handle}</div>
           </header>
           <main>
-            ${tweetData.content.text}
+            ${tweetText}
           </main>
           <footer>
             <div class="time">
@@ -53,10 +86,11 @@ $(document).ready(function(){
         </article>`
   }
 
+  function renderTweets() {
+    data.forEach(function(tweetData) {
+      $('#tweets-container').append(createTweetElement(tweetData));
+    })
+  };
 
-  console.log(createTweetElement(tweetData));
-
-  // Test / driver code (temporary)
-  // to see what it looks like
-  $('#tweets-container').append(createTweetElement(tweetData)); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
 });
