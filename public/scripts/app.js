e5e3ded89ec3b2563abfbdcf9c88dc2a989d3516
chrynuik <catherine.hrynuik@gmail.com>
2017-11-15 20:43:03 +0000

    ajax loaded tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,52 +6,7 @@
 
 
 $(document).ready(function(){
-  var data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants <script>alert('Vulnerable')</script>"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
+
   function escape(str) {
     var div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
@@ -62,7 +17,7 @@ $(document).ready(function(){
 
     var tweetTime = Math.floor((Date.now() - tweetData.created_at) / (1000*60*60*24));
     var tweetText = escape(tweetData.content.text);
-    console.log(tweetData);
+    // console.log(tweetData);
 
     return `<article class="tweet">
           <header>
@@ -86,7 +41,7 @@ $(document).ready(function(){
         </article>`
   }
 
-  function renderTweets() {
+  function renderTweets(data) {
     data.forEach(function(tweetData) {
       $('#tweets-container').append(createTweetElement(tweetData));
     })
@@ -95,7 +50,7 @@ $(document).ready(function(){
   function handleNewTweet(event){
     event.preventDefault();
     const $form = $(this);
-    console.log($form.serialize());
+    // console.log($form.serialize());
    // $.ajax ({
    //  type: 'POST',
    //  url: '/',
@@ -114,6 +69,16 @@ $(document).ready(function(){
 
   $form.on('submit', handleNewTweet);
 
-  renderTweets(data);
+  function loadTweets() {
+    $.getJSON('/tweets')
+      .done ((tweets) => {
+        console.log(tweets);
+      })
+      .done ((tweets) => {
+        renderTweets(tweets);
+      })
+  }
+
+  loadTweets();
 });
 
